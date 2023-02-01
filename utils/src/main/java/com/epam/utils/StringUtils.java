11b7a2e2e4e5b9d23c11fc66386a0bf418a51523
foxstudent103976 <teona.odisharia@gmail.com>
2023-02-01 09:03:14 +0400

    task-2 v05

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,10 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        return org.apache.commons.lang3.StringUtils.isNumeric(str) && Double.parseDouble(str) % 2 == 0 && Double.parseDouble(str) > 0;
+        if (!org.apache.commons.lang3.StringUtils.isNumeric(str)) {
+            return false;
+        }
+        int number = Integer.parseInt(str);
+        return number > 0 && number % 2 == 0;
     }
 }
