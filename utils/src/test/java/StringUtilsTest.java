10c6cbdf341297a42248fed4b87f1310dd9efa71
lucciolla200 <teoteo.odisharia@gmail.com>
2023-11-26 15:29:57 +0400

    solution 7

--- utils/src/test/java/StringUtilsTest.java
+++ utils/src/test/java/StringUtilsTest.java
@@ -1,22 +1,37 @@
-
-import com.epam.utils.StringUtils;
+package com.epam.utils;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class StringUtilsTest {
+
     @Test
     public void testIsPositiveNumberWithPositiveNumber() {
-        assertTrue(StringUtils.isPositiveNumber("12"));
+        assertTrue(StringUtils.isPositiveNumber("12"), "12 should be considered a positive number");
     }
 
     @Test
     public void testIsPositiveNumberWithNegativeNumber() {
-        assertFalse(StringUtils.isPositiveNumber("-5"));
+        assertFalse(StringUtils.isPositiveNumber("-5"), "-5 should not be considered a positive number");
+    }
+
+    @Test
+    public void testIsPositiveNumberWithZero() {
+        assertFalse(StringUtils.isPositiveNumber("0"), "0 should not be considered a positive number");
     }
 
     @Test
     public void testIsPositiveNumberWithNonNumericString() {
-        assertFalse(StringUtils.isPositiveNumber("abc"));
+        assertFalse(StringUtils.isPositiveNumber("abc"), "Non-numeric string should not be considered a positive number");
+    }
+
+    @Test
+    public void testIsPositiveNumberWithEmptyString() {
+        assertFalse(StringUtils.isPositiveNumber(""), "Empty string should not be considered a positive number");
+    }
+
+    @Test
+    public void testIsPositiveNumberWithNull() {
+        assertFalse(StringUtils.isPositiveNumber(null), "Null should not be considered a positive number");
     }
 }
